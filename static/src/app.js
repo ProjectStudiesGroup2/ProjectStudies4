d88e1af37db8d56bef8d19abf1698169b9194928
HYDROGEN\Dimitrios <mrhydro7k@gmail.com>
2016-12-07 17:27:07 +0200

    Cleaned up project files, code in Client directory and fixed some minor warnings. Started adding extra tab, content *and gonna fix more warning soon*

--- static/src/app.js
+++ static/src/app.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react'
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory, DefaultRoute, IndexLink } from 'react-router'
+import React from 'react'
+import { Router, Route, Link, IndexRoute, hashHistory, IndexLink } from 'react-router'
 
-class App extends Component {
+class App extends React.Component {
   render () {
     return (
       <Router history={hashHistory}>
         <Route path='/' component={Container}>
           <IndexRoute component={Home} />
           <Route path='/address' component={Address}>
-            <IndexRoute component={TwitterFeed} />
-            <Route path='instagram' component={Instagram} />
+            <IndexRoute component={Categ1} />
+            <Route path='cat2' component={Categ2} />
           </Route>
+          <Route path='/search' component={Search} />
           <Route path='/about' component={About} />
           <Route path='*' component={NotFound} />
         </Route>
@@ -22,28 +23,28 @@ class App extends Component {
 const Nav = () => (
   <div>
     <IndexLink activeClassName='active' to='/'>Home</IndexLink>&nbsp;
-    <IndexLink activeClassName='active' to='/address'>Address</IndexLink>&nbsp;
+    <IndexLink activeClassName='active' to='/address'>Categories</IndexLink>&nbsp;
+    <IndexLink activeClassName='active' to='/search'>Search</IndexLink>&nbsp;
     <IndexLink activeClassName='active' to='/about'>About</IndexLink>
   </div>
 )
 
-const Container = (props) => <div>
-  <Nav />
-  {props.children}
-</div>
+const Container = (props) => <div><Nav />{props.children}</div>
 
 const Home = () => <h1>Hello from Home!</h1>
 
 const Address = (props) => <div>
   <br />
-  <Link to='/address'>Twitter Feed</Link>&nbsp;
-  <Link to='/address/instagram'>Instagram Feed</Link>
-  <h1>We are located at 555 Jackson St.</h1>
+  <Link to='/address'>Categories</Link>&nbsp;
+  <Link to='/address/cat2'>More categories</Link>
+  <h1>Category page:</h1>
   {props.children}
 </div>
 
-const Instagram = () => <h3>Instagram Feed</h3>
-const TwitterFeed = () => <h3>Twitter Feed</h3>
+const Categ1 = () => <h3>Some categories here</h3>
+const Categ2 = () => <h3>Some more categories here</h3>
+
+const Search = () => <h2>Coming soon</h2>
 
 const About = () => <h3>Welcome to the About Page</h3>
 
--- static/src/index.js
+++ static/src/index.js
@@ -1,20 +1,8 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// // import {ReactRouter, Router, Route} from 'react-router';
-// import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
-// import MainP from './MainP';
-
-// ReactDOM.render(
-//   <ReactRouter.Router>
-//     <ReactRouter.Route path="./static" component={MainP}>
-
-//     </ReactRouter.Route>
-//   </ReactRouter.Router>,
-//   document.getElementById('mainP')
-// );
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />,
+document.getElementById('root')
+);
