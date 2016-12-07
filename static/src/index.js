0510ee6298e339f9e31d01fe9b46b80f6731b392
Roman Kucherenko <roman@idontgiveadamn.com>
2016-12-07 15:16:57 +0200

    Routing implemented

--- static/src/index.js
+++ static/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import {ReactRouter, Router, Route} from 'react-router';
-import MainP from './MainP';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import {ReactRouter, Router, Route} from 'react-router';
+// import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
+// import MainP from './MainP';
 
-ReactDOM.render(
-  <ReactRouter.Router>
-    <ReactRouter.Route path="./static" component={MainP}>
+// ReactDOM.render(
+//   <ReactRouter.Router>
+//     <ReactRouter.Route path="./static" component={MainP}>
 
-    </ReactRouter.Route>
-  </ReactRouter.Router>,
-  document.getElementById('mainP')
-);
+//     </ReactRouter.Route>
+//   </ReactRouter.Router>,
+//   document.getElementById('mainP')
+// );
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+
+ReactDOM.render(<App />, document.getElementById('root'))
