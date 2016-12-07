aea6d0903067c746a1190fc9352f7de469ced014
HYDROGEN\Dimitrios <mrhydro7k@gmail.com>
2016-12-07 11:12:40 +0200

    Trying out webpack to fix issues

--- static/src/index.js
+++ static/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {ReactRouter, Router, Route} from 'react-router';
+// import {ReactRouter, Router, Route} from 'react-router';
 import MainP from './MainP';
-import Nav from './MainP';
 
 ReactDOM.render(
   <ReactRouter.Router>
-    <ReactRouter.Route path="/" component={MainP}>
+    <ReactRouter.Route path="./static" component={MainP}>
 
     </ReactRouter.Route>
   </ReactRouter.Router>,
