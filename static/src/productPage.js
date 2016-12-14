aaf0c2651bee9a373991a6d14327e730bdfeaa40
Roman Kucherenko <roman@idontgiveadamn.com>
2016-12-14 15:01:30 +0200

    Added tabs for dedcription of a product

--- static/src/productPage.js
+++ static/src/productPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import DocumentTitle from 'react-document-title'
- 
+
 export default class Product extends React.Component {
   render() {
     return (
@@ -18,8 +18,100 @@ export default class Product extends React.Component {
               September smallness northward situation few her certainty something.</p>
             </div>
           </div>
+          <div className="row margin-top">
+            <div className="col-xs-12">
+              <Tabs>
+                <Pane label="Tab 1">
+                  <div>This is my tab 1 contents!</div>
+                </Pane>
+                <Pane label="Tab 2">
+                  <div>This is my tab 2 contents!</div>
+                </Pane>
+                <Pane label="Tab 3">
+                  <div>This is my tab 3 contents!</div>
+                </Pane>
+              </Tabs>
+            </div>
+          </div>
         </div>
       </DocumentTitle>
     );
   }
 }
+
+const Tabs = React.createClass({
+  displayName: 'Tabs',
+  propTypes: {
+    selected: React.PropTypes.number,
+    children: React.PropTypes.oneOfType([
+      React.PropTypes.array,
+      React.PropTypes.element
+    ]).isRequired
+  },
+  getDefaultProps() {
+    return {
+      selected: 0
+    };
+  },
+  getInitialState() {
+    return {
+      selected: this.props.selected
+    };
+  },
+  handleClick(index, event) {
+    event.preventDefault();
+    this.setState({
+      selected: index
+    });
+  },
+  _renderTitles() {
+    function labels(child, index) {
+      let activeClass = (this.state.selected === index ? 'active' : '');
+      return (
+        <li key={index}>
+          <a href="#" 
+            className={activeClass}
+            onClick={this.handleClick.bind(this, index)}>
+            {child.props.label}
+          </a>
+        </li>
+      );
+    }
+    return (
+      <ul className="tabs__labels">
+        {this.props.children.map(labels.bind(this))}
+      </ul>
+    );
+  },
+  _renderContent() {
+    return (
+      <div className="tabs__content">
+        {this.props.children[this.state.selected]}
+      </div>
+    );
+  },
+  render() {
+    return (
+      <div className="tabs">
+        {this._renderTitles()}
+        {this._renderContent()}
+      </div>
+    );
+  }
+});
+
+const Pane = React.createClass({
+  displayName: 'Pane',
+  propTypes: {
+    label: React.PropTypes.string.isRequired,
+    children: React.PropTypes.element.isRequired
+  },
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+ 
