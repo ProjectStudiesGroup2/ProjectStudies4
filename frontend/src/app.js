b7e5685c6914ce3209e67f6796f7408e8e6a1477
HYDROGEN\Dimitrios <mrhydro7k@gmail.com>
2016-12-16 01:16:43 +0200

    ...

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -16,6 +16,10 @@ var instance = axios.create({
   baseURL: 'https://g2p4.herokuapp.com/api',
   headers: {'Access-Control-Allow-Origin': 'http://localhost:3000'}
 });
+instance.get('/categories')
+.then(function (response){
+  console.log(response)
+});
 
 const NotFound = () => (
   <div className="container-fluid">
