03919092e4ad7a71582e31d7a7377dd80a00deda
Roman Kucherenko <roman@idontgiveadamn.com>
2016-12-16 00:23:22 +0200

    fix merging problems

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -4,7 +4,6 @@ import ProdSlider from './productSlider'
 import DocumentTitle from 'react-document-title'
 
 const Home = () => (
-<<<<<<< HEAD:static/src/home.js
     <DocumentTitle title={`Web Store - Home page`} className="container">
         <div id="wrapper"> 
             <div className="content">
@@ -25,26 +24,6 @@ const Home = () => (
             </div> 
         </div> 
     </DocumentTitle>
-=======
-    <div id="wrapper">
-        <div className="content">
-            <SimpleSlider />
-            <div className="container margin-top">
-                <h4>Most viewed </h4>
-            </div>
-            <ProdSlider />
-        </div>
-        <div id="footer">
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-7">
-                        <p>Something here</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
->>>>>>> develop:frontend/src/home.js
 )
 
 
